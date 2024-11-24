d8a3fb0719fd43d12edc7b3f3a4ecda83e22ea22
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-11-24 16:46:29 +0500

    transform true enabled

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,10 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api/v1');
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true
+  }));
 
   app.enableCors({
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
